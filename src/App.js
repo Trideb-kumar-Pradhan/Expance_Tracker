89269e34f86072fa2cbb370f802f34182e23b796
PritamWankhede <pritamwankhede671@gmail.com>
2024-04-06 13:17:03 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import MainPage from "./Component/MainPage/MainPage";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <MainPage />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/MainPage/MainPage.jsx
@@ -0,0 +1,128 @@
+import React, { useState, useEffect } from "react";
+import AddBalance from "../AddBalance/AddBalance";
+import AddExpences from "../AddExpences/AddExpences";
+import Table from "../Table/Table";
+import ExpencesPieChart from "../Chart/ExpencesPieChart";
+import BarChartComponent from "../Chart/BarChartExpences";
+import Styles from "./MainPage.module.css";
+import { SnackbarProvider } from "notistack";
+import { Bar } from "recharts";
+
+const MainPage = () => {
+  const initializeBalAmount = () => {
+    const storedAmount = localStorage.getItem("balAmount");
+    return storedAmount ? parseInt(storedAmount) : 5000;
+  };
+  const [balAmount, setBalAmount] = useState(initializeBalAmount);
+
+  const [totalExpenses, setTotalExpenses] = useState(() => {
+    const storedTotalExpenses = localStorage.getItem("totalExpenses");
+    return storedTotalExpenses ? parseFloat(storedTotalExpenses) : 0;
+  });
+  const [expenses, setExpenses] = useState(() => {
+    const storedExpenses = localStorage.getItem("expenses");
+    return storedExpenses ? JSON.parse(storedExpenses) : [];
+  });
+
+  const [editExpense, setEditExpense] = useState(null);
+
+  useEffect(() => {
+    localStorage.setItem("totalExpenses", totalExpenses);
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  }, [totalExpenses, expenses]);
+
+  const handleExpenses = (newExpense) => {
+    setExpenses([...expenses, newExpense]);
+    setTotalExpenses(totalExpenses + newExpense.price);
+  };
+
+  const handleDeleteExpense = (updatedExpenses) => {
+    try {
+      const deletedExpensesTotal = updatedExpenses.reduce(
+        (total, expense) => total + expense.price,
+        0
+      );
+      localStorage.setItem("totalExpenses", deletedExpensesTotal.toString());
+
+      setExpenses(updatedExpenses);
+      setTotalExpenses(deletedExpensesTotal); 
+    } catch (error) {
+      console.error("Error updating total expenses:", error);
+    }
+  };
+
+  const handleEditExpense = (expense) => {
+    setEditExpense(expense);
+  };
+
+  const handleUpdateExpense = (updatedExpense) => {
+    const updatedExpenses = expenses.map((expense) =>
+      expense === editExpense ? updatedExpense : expense
+    );
+    setExpenses(updatedExpenses);
+    setEditExpense(null);
+    const difference = updatedExpense.price - editExpense.price;
+    setTotalExpenses(totalExpenses + difference);
+  };
+
+  const calculateCategorySpending = () => {
+    const categorySpending = {};
+    expenses.forEach((expense) => {
+      const { category, price } = expense;
+      if (categorySpending[category]) {
+        categorySpending[category] += price;
+      } else {
+        categorySpending[category] = price;
+      }
+    });
+    return Object.entries(categorySpending).map(([category, value]) => ({
+      name: category,
+      value: value,
+    }));
+  };
+
+  return (
+    <div>
+      <h1 className={Styles.Heading}>Expense Tracker</h1>
+      <div className={Styles.TopContainer}>
+        <div className={Styles.Child}>
+          <SnackbarProvider>
+            <AddBalance balAmount={balAmount} setBalAmount={setBalAmount} />
+          </SnackbarProvider>
+        </div>
+        <div className={Styles.Child}>
+          <SnackbarProvider>
+            <AddExpences
+              balAmount={balAmount}
+              setBalAmount={setBalAmount}
+              onAddExpenses={handleExpenses}
+              totalExpenses={totalExpenses}
+              editExpense={editExpense}
+              setEditExpense={setEditExpense}
+              onUpdateExpense={handleUpdateExpense}
+            />
+          </SnackbarProvider>
+        </div>
+        <div className={Styles.Child}>
+          <ExpencesPieChart categoriesData={expenses} />
+        </div>
+      </div>
+      <div className={Styles.BottomContainer}>
+        <div className={Styles.BottomChild1}>
+          <SnackbarProvider>
+            <Table
+              expenses={expenses}
+              onDeleteExpense={handleDeleteExpense}
+              onEditExpense={handleEditExpense}
+            />
+          </SnackbarProvider>
+        </div>
+        <div className={Styles.BottomChild2}>
+          <BarChartComponent data={calculateCategorySpending()} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MainPage;
